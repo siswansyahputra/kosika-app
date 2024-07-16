ad52bba5733993b2839e2b72bb1aee780a07ea98
Siswan Syahputra <siswansyahputra90@gmail.com>
2024-07-16 15:13:09 +0700

    upload

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Login from "./login/page";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-slate-100">
+      <Login />
+    </div>
+  );
+}
